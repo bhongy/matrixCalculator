49a162768fddabbbf98d1ad14aa2c921c098b847
Thanik Bhongbhibhat <thanik@dropcam.com>
2014-06-21 17:51:24 -0700

    Initail Commit

---
+++ matrix-calculator.js
@@ -0,0 +1,16 @@
+/*
+Matrix = | a c e | = | 1 0 0 |
+         | b d f |   | 0 1 0 |
+*/
+
+var a = 1,
+    b = 0,
+    c = 0,
+    d = 1,
+    e = 0,
+    f = 0,
+    matrixString = '';
+
+// modify value from translateX
+
+// 
